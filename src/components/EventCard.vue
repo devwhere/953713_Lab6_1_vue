f4e7d63b2706258b017d18e1803e4577917fd2d4
pattama422 <pattama@camt.info>
2026-03-26 14:38:46 +0700

    read data form file

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,30 +1,28 @@
 <script setup lang="ts">
-withDefaults(
-  defineProps<{
-    name: string
-  }>(),
-  {
-    name: 'default name',
+import type { Event } from '@/types'
+import { ref } from 'vue'
+
+const event = ref<Event>({
+  id: 1,
+  category: 'Category 1',
+  title: 'Event 1',
+  description: 'Description of Event 1',
+  date: '2021-10-10',
+  time: '10:00',
+  location: 'Location 1',
+  petsAllowed: true,
+  organizer: {
+    name: 'Organizer 1',
   },
-)
+})
 </script>
 
 <template>
-  <div class="event-card">
-    <h1>This is an {{ name }} page</h1>
+  <div class="event-class">
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <p>{{ event.description }}</p>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
   </div>
 </template>
-
-<style scoped>
-.event-card {
-  padding: 20px;
-  width: 250px;
-  cursor: pointer;
-  border: 1px solid #39495c;
-  margin-bottom: 18px;
-}
-.event-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
-</style>
