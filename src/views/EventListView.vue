d90036ba863134961f041b03b4507525ed85bd12
pattama422 <pattama@camt.info>
2026-03-26 13:04:12 +0700

    add vue component

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,5 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+</script>
 
 <template>
-  <div></div>
+  <div class="event-list">
+    <EventCard />
+    <EventCard />
+    <EventCard />
+  </div>
 </template>
+
+<style scoped>
+.event-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
